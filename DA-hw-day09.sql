b36a8a9aaccd6c6f0eba77fe7322f38fcf08d30c
Pham Quan <73213386+Madmjce@users.noreply.github.com>
2024-03-20 00:14:35 +0700

    Create DA-hw-day09.sql

---
+++ DA-hw-day09.sql
@@ -0,0 +1,25 @@
+--ex1: datalemur-laptop-mobile-viewership.
+SELECT
+    SUM(
+        CASE
+            WHEN device_type = 'laptop' THEN 1 ELSE 0
+        END
+    ) AS laptop_views,
+    SUM(
+        CASE
+        WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0
+        END
+    ) AS mobile_views
+FROM
+    viewership;
+--ex2: 
+SELECT 
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No'
+    END AS 'triangle'
+FROM
+    Triangle
